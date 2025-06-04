3e7022942e8ce044d516d6fa59abcd02a2607722
alexljn5 <alexljn5@gmail.com>
2025-06-04 02:43:52 -0400

    tt

--- game_engine/raycasting.js
+++ game_engine/raycasting.js
@@ -4,8 +4,8 @@ import { textureIdMap, floorTextureIdMap } from "./mapdata/maptextures.js";
 import { CANVAS_WIDTH } from "./globals.js";
 
 export let playerFOV = Math.PI / 6; // 60 degrees
-export let numCastRays = 1000; // Reduced for performance
-export let maxRayDepth = 8;
+export let numCastRays = 400; // Reduced for performance
+export let maxRayDepth = 11;
 
 // --- OPTIMIZED RAYCASTING WORKER MANAGEMENT ---
 const NUM_WORKERS = 4;
