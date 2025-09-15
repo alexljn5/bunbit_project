9779de505e8cd5b937615c0e3fdb48127311d051
alexljn5 <alexljn5@gmail.com>
2025-09-15 13:46:57 +0200

    l

--- game_engine/preload.js
+++
@@ -1,9 +0,0 @@
-import { contextBridge } from "electron";
-import { readFileSync } from "fs";
-
-const pkg = JSON.parse(readFileSync("./package.json", "utf8"));
-
-contextBridge.exposeInMainWorld("gameInfo", {
-    name: pkg.name,
-    version: pkg.version
-});
